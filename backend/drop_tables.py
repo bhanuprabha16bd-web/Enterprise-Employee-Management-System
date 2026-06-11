0d09e72bdad41c4af4a9316be29a0b1beea9c1ce
bhanuprabha16bd-web <bhanuprabha16bd@gmail.com>
2026-06-11 12:51:10 +0530

    Delete backend/drop_tables.py

--- backend/drop_tables.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
-from app.database.config import engine, Base
-from app.models import user_db, department_db, employee_db, role_request_db, company_db, audit_log_db
-
-Base.metadata.drop_all(bind=engine)
-print("All tables dropped successfully.")
